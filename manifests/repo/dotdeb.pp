ea1d691a581776487e60284f05850def1e75e74d
Dolf Schimmel <dschimmel@transip.nl>
2013-12-18 13:15:11 +0100

    Adding php55 and fw rules to dotdeb repo class

--- manifests/repo/dotdeb.pp
+++ manifests/repo/dotdeb.pp
@@ -2,7 +2,10 @@
 #
 # This class installs the dotdeb repo
 #
-class apt::repo::dotdeb {
+class apt::repo::dotdeb (
+  $enable_php55 = false,
+  $firewall     = params_lookup('firewall', 'global' )
+) {
 
   case $::operatingsystem {
   Debian: {
@@ -10,8 +13,36 @@ class apt::repo::dotdeb {
       url        => 'http://packages.dotdeb.org/',
       distro     => $::lsbdistcodename,
       repository => 'all',
-      key        => '89DF5277',
+      key_url    => 'http://www.dotdeb.org/dotdeb.gpg',
+      key        => '4096R/89DF5277',
     }
+
+    if any2bool($enable_php55) {
+      apt::repository { 'dotdeb-php55':
+        url        => 'http://packages.dotdeb.org',
+        distro     => 'wheezy-php55',
+        repository => 'all',
+        key_url    => 'http://www.dotdeb.org/dotdeb.gpg',
+        key        => '4096R/89DF5277',
+      }
+    }
+
+    if any2bool($firewall) {
+      firewall::rule { 'dotdeb-apt':
+        protocol        => 'tcp',
+        port            => '80',
+        destination     => [ 'packages.dotdeb.org', 'www.dotdeb.org' ],
+        destination_v6  => [ 'packages.dotdeb.org', 'www.dotdeb.org' ],
+        direction       => 'output',
+        before          => Service[$::Firewall::Setup::service_name]
+      }
+
+      Service[$::Firewall::Setup::service_name] -> Apt::Repository[ 'dotdeb' ]
+      if any2bool($enable_php55) {
+        Service[$::Firewall::Setup::service_name] -> Apt::Repository[ 'dotdeb-php55' ]
+      }
+    }
+
   }
   default: {}
   }
